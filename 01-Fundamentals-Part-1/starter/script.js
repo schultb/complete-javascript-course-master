539a457e4cc599bc3820b8ccf9a01609aef9c3f1
Ben Schultze <ben_schultze@outlook.com>
2021-01-20 13:06:43 -0500

    upload

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,6 +3,23 @@
 
 
 // Coding Challenge #1
+// let markMass = 95;
+// let markHeight = 1.88
+// let johnMass = 85;
+// let johnHeight = 1.76;
+
+// var bmiMark = markMass / markHeight ** 2;
+// var bmiJohn = johnMass / johnHeight ** 2;
+
+// console.log(bmiMark, bmiJohn);
+
+// var markHigherBMI = bmiMark > bmiJohn;
+
+// console.log(markHigherBMI);
+
+
+// Coding Challenge #2
+
 let markMass = 95;
 let markHeight = 1.88
 let johnMass = 85;
@@ -11,8 +28,19 @@ let johnHeight = 1.76;
 var bmiMark = markMass / markHeight ** 2;
 var bmiJohn = johnMass / johnHeight ** 2;
 
-console.log(bmiMark, bmiJohn);
-
 var markHigherBMI = bmiMark > bmiJohn;
 
-console.log(markHigherBMI);
+console.log(bmiMark, bmiJohn);
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI is higher than John's!`)
+} else {
+    console.log(`John's BMI is higher than Mark's!`)
+};
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI, ${bmiMark}, is higher than John's, ${bmiJohn}.`)
+} else {
+    console.log(`John's BMI, ${bmiJohn}, is higher than Mark's, ${bmiMark}.`)
+};
+
