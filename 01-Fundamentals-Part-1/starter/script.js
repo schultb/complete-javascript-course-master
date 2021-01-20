f9801e47b7c3ba22c42e3f0137bdedaaca848707
Ben Schultze <ben_schultze@outlook.com>
2021-01-20 10:28:07 -0500

    upload

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,18 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is fun!");
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is fun!");
+
+
+// Coding Challenge #1
+let markMass = 95;
+let markHeight = 1.88
+let johnMass = 85;
+let johnHeight = 1.76;
+
+var bmiMark = markMass / markHeight ** 2;
+var bmiJohn = johnMass / johnHeight ** 2;
+
+console.log(bmiMark, bmiJohn);
+
+var markHigherBMI = bmiMark > bmiJohn;
+
+console.log(markHigherBMI);
