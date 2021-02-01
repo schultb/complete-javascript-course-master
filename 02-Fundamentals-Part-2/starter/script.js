a348e8f26e1749cdef34f9ce7b0858c55c015a99
Ben Schultze <ben_schultze@outlook.com>
2021-02-01 15:17:26 -0500

    upload

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -123,3 +123,57 @@ const higher = function () {
 }
 
 console.log(higher());
+
+// ----------------------------------------------------------------------------------------------------
+
+// Coding Challenge #4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+}
+/* console.log(tips); */
+
+for (let i = 0; i < tips.length; i++) {
+    totals.push(bills[i] + tips[i]);
+}
+console.log(tips, totals);
+
+// -----------------------------------------------------------------------------------------------------------
+
+// A more refined way of writing Coding Challenge #4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+/* Bonus */
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+        /* Another way of writing the above formula */
+        /* sum+= arr[i]; */
+    }
+    console.log(sum);
+    return sum / arr.length;
+}
+console.log(calcAverage(totals));
