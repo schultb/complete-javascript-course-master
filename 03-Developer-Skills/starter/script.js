c7749bf5b845d4e361a77c2c2122ccb7b9f5e7c4
Ben Schultze <ben_schultze@outlook.com>
2021-02-08 11:35:50 -0500

    upload

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,2 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]} Celsius in ${i + 1} days ... `;
+  }
+  console.log("... " + str);
+};
+printForecast(data1);
+printForecast(data2);
