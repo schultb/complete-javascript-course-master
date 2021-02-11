eec6d0806336191346b2e5cc85d08bbbb1f53dee
Ben Schultze <ben_schultze@outlook.com>
2021-02-11 14:37:58 -0500

    upload

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,10 +16,20 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
+  //When there is no input.
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    // When player wins.
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.number').textContent = secretNumber;
+    // Change background color in CSS when player wins
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    // Change style width on number
+    document.querySelector('.number').style.width = '30rem';
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
@@ -29,6 +39,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low.
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low!';
