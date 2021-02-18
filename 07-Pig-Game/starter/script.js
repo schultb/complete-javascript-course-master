56856015fce5f80ff76696f28c2a9c6328a7291d
Ben Schultze <ben_schultze@outlook.com>
2021-02-18 14:06:41 -0500

    upload

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,8 +3,36 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const current0E1 = document.getElementById('current--0');
+const current1E1 = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //   Check for rolled 1
+  if (dice !== 1) {
+    //   Add dice to current score
+    currentScore += currentScore + dice;
+    current0E1.textContent = currentScore;
+  } else {
+    //   switch player
+  }
+});
