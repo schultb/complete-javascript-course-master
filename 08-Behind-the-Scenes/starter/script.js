55d712ca04fb468ff08887ad80d68afd88ebd7d5
Ben Schultze <ben_schultze@outlook.com>
2021-02-23 15:30:49 -0500

    upload

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2020 - birthYear;
+  return age;
+}
+
+const firstName = 'Ben';
+calcAge(1991);
+console.log(calcAge(1974));
+console.log(firstName);
