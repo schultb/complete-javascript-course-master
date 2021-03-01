1986ae2dd577f4a88e9897905d88a229792b20ad
schultb <ben_schultze@outlook.com>
2021-03-01 11:50:52 -0500

    upload

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,3 +1,9 @@
+/* const arr = [2, 3, 4];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z); */
+
 'use strict';
 
 const restaurant = {
@@ -7,7 +13,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  /* openingHours: {
     thu: {
       open: 12,
       close: 22,
@@ -20,5 +30,47 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
-  },
+  }, */
 };
+
+// *********************************************************************
+
+/* const arr = [2, 3, 4];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z); */
+// *******************************************************************
+
+/* const [first, second, third] = restaurant.categories;
+console.log(first, second, third); */
+
+/* Empty portion with comma skips the second one in the list and moves to the third. */
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+/* This allows you to change property so that Vegetarian now comes before Italian. Switching variables*/
+/* const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary); */
+
+/* Easier and better way to switch array order */
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(restaurant.order(2, 0));
+/* Destructuring version--How we receive 2 return values from a function */
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+/* Destructuring array within an array as separate variables */
+const nested = [2, 4, [5, 6]];
+/* const [i, , j] = nested;
+console.log(i, j); */
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+/* Default Values */
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
