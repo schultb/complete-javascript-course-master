6f4c362e49aaa494b85962212ea33b3714a2177f
Ben Schultze <ben_schultze@outlook.com>
2021-10-11 15:44:41 -0400

    commit

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,7 +50,7 @@ ordersSet.add('Garlic Bread');
 ordersSet.add('Garlic Bread');
 console.log(ordersSet);
 ordersSet.delete('Risotto');
-// ordersSet.clear();
+ordersSet.clear();
 console.log(ordersSet);
 
 for (const order of ordersSet) console.log(order);
@@ -62,4 +62,33 @@ console.log(staffUnique);
 console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
 );
-console.log(new Set('Schultze').size);
+// console.log(new Set('Schultze').size);
+
+// Maps
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', [('Italian', 'Pizzeria', 'Vegetarian', 'Organic')])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 6;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'test');
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
